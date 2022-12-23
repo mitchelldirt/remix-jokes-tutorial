8c648ef4d984922f3b94bcd7835b9045736c6d22
Mitchell Mudd <mitchellmudd@outlook.com>
2022-12-22 21:14:30 -0500

    completed database section 💽

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,28 +1,64 @@
 import type { LinksFunction } from "@remix-run/node";
-import { Link, Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
 
-import stylesUrl from "~/styles/index.css";
+import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function IndexRoute() {
+export const loader = async () => {
+  return json({
+    jokeListItems: await db.joke.findMany(),
+  });
+};
+
+export default function JokesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div className="container">
-      <div className="content">
-        <h1>
-          Remix <span>Jokes!</span>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="jokes">Read Jokes</Link>
-              <Outlet />
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link
+              to="/"
+              title="Remix Jokes"
+              aria-label="Remix Jokes"
+            >
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
